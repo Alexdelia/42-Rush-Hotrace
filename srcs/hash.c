7c4c579bc341d3af0aa61f2c0a381ec1fed64751
Alexdelia <alexandre.delille.57@gmail.com>
2021-12-11 19:23:38 +0100

    clean hashing

--- srcs/hash.c
+++ srcs/hash.c
@@ -6,26 +6,18 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 18:20:43 by adelille          #+#    #+#             */
-/*   Updated: 2021/12/09 18:30:03 by adelille         ###   ########.fr       */
+/*   Updated: 2021/12/11 19:22:09 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/hotrace.h"
 
-size_t	get_hash(const char *tag)
+size_t	get_hash(const char *key)
 {
 	size_t	hash;
-	size_t	i;
-	int		c;
 
 	hash = HASH;
-	i = 0;
-	while (tag[i])
-	{
-		c = tag[i];
-		i++;
-		hash = ((hash << 5) + hash) + c;
-	}
-	hash %= TABLE_SIZE;
-	return (hash);
+	while (*key)
+		hash = hash * 101 + *key++;
+	return (hash % TABLE_SIZE);
 }
