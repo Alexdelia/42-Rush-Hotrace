53e0915e2f2ffdd4ea3838a2cb22c952e3c992f4
adelille <adelille@student.42.fr>
2021-12-12 16:23:31 +0100

    remove dead code

--- srcs/utils/ft_strdup.c
+++ srcs/utils/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 16:46:28 by adelille          #+#    #+#             */
-/*   Updated: 2021/12/11 17:34:46 by adelille         ###   ########.fr       */
+/*   Updated: 2021/12/12 16:23:09 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,22 +29,3 @@ char	*ft_strdup_n(const char *src, const size_t size)
 	res[i] = '\0';
 	return (res);
 }
-
-/*char	*ft_strdup_hotrace(size_t base, const size_t size)
-{
-	char	*str;
-	size_t	i;
-
-	str = (char *)malloc(sizeof(char) * (size - base + 2));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (base < size)
-	{
-		str[i] = g_d.stdin[base];
-		i++;
-		base++;
-	}
-	str[i] = '\0';
-	return (str);
-}*/
